80c578ee25f7d8d46c692561c0a1b6e79d8115c7
Suat Zengin <zenginsuat.inf@gmail.com>
2021-05-16 12:08:14 +0300

    added models

--- library_project_con/library_app/admin.py
+++ library_project_con/library_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Book, BookBorrow, Author, Genre, Language
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(BookBorrow)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
--- library_project_con/library_app/models.py
+++ library_project_con/library_app/models.py
@@ -1,3 +1,82 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
+from datetime import date
 # Create your models here.
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.genre
+
+
+class Language(models.Model):  # book's language
+    language = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.language
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    isbn = models.CharField(
+        'ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    about = models.TextField(max_length=1000, blank=True, null=True)
+    number_of_page = models.SmallIntegerField()
+    book_genre = models.ManyToManyField(
+        Genre, help_text='Select a genre for this book')
+    book_language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True, blank=True)
+    # author
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def short_about(self):
+        return self.about[:50]
+
+
+LOAN_STATUS = (
+    ('m', 'Maintance'),
+    ('o', 'On Loan'),
+    ('a', 'Available'),
+    ('r', 'Reserved'),
+)
+
+
+class BookBorrow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
+    due_back = models.DateField(blank=True, null=True)
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        default='a',
+        help_text='Book availability'
+    )
+
+    def __str__(self):
+        return "{0} | {1}".format(self.book.title, self.id)
+
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+    class Meta:
+        ordering = ['due_back']
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_death = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
